c688943f54e376bd88e83cd210af91999ea1f19b
onyoo <roberto9m9@gmail.com>
2016-02-01 00:49:57 +0100

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function addIngredient(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function removeIngredient(object, key) {
+  delete object[key];
+  return object;
+}
+
+function updateIngredient(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function readRecipe(object) {
+  if (Object.keys(object).length > 0) {
+    for (var key in object) {
+      console.log("this recipe calls for " + object[key] + " of " + key)
+    }
+  }
+}
